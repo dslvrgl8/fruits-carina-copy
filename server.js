7a41afd09430a963b3aed7efc3a13e9d84fa181c
Joe Keohan <jkeohan@gmail.com>
2023-03-20 17:40:20 -0400

    cleaned up code from previous class

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-// These are just setting up an instance of express. Literally directly from Express' documentation. 
 const express = require('express');
 const app = express();
 const port = 4000
 const fruitsController = require('./controllers/fruits');
-// console.log(fruitsController)
 
-// This is going to the Fruits.js file in the models directory. It is set equal to the export from that file
-const models = require('./models/Fruits');
-// console.log(models);
-// This is getting just the array of fruits. Not the whole object that has two key/value pairs.
-const fruits = models.fruits
 // Models - Database stuff
+const models = require('./models/Fruits');
+
+const fruits = models.fruits
+
+
 // controllers - routes
 // views - EJS files (EJS is literally just HTML and JS)
 
 // Middleware 
-// This is a view engine that is looking for EJS files to be rendered. It also sets up that ALL the EJS files for my frontend will be located in a file named views
 app.set('view engine', 'ejs');
 
 
@@ -30,11 +27,6 @@ app.get('/api', (req, res) => {
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
-})
-
-// This is just my index route. I want this to be where I have a list of all my data. For example, all my products or fruits or star wars ships
-app.get('/starwars', (req, res) => {
-    res.render('starwars/index.ejs')
 })
 
 app.use('', fruitsController);
